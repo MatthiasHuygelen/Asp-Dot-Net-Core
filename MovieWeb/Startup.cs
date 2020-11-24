b7bc859f22bcebe86f08c792b6d0301f48cd310d
Matthias Huygelen <Matthias.Huygelen@realdolmen.com>
2020-11-24 08:59:12 +0100

    qr code

--- MovieWeb/Startup.cs
+++ MovieWeb/Startup.cs
@@ -47,7 +47,7 @@ namespace MovieWeb
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error" );
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
